5e6b9a2c5cbbfc690b275c78bc6216a343691191
Doorslayer <kaylehb8@gmail.com>
2023-04-27 00:34:15 -0700

    fixed query to disclude unplayable cards

--- app/app.js
+++ app/app.js
@@ -4,14 +4,14 @@ function nextCard() {
     //get random card
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        let card = JSON.parse(xhttp.responseText);
-        localStorage.setItem('card', card);
-        document.getElementById("cardImage").src = card.image_uris.normal;
-      }
+        if (this.readyState == 4 && this.status == 200) {
+            let card = JSON.parse(xhttp.responseText);
+            localStorage.setItem('card', card.name);
+            document.getElementById("cardImage").src = card.image_uris.normal;
+        }
     };
-    xhttp.open("GET", "https://api.scryfall.com/cards/random", true);
+    xhttp.open("GET", "https://api.scryfall.com/cards/random?q=in%3Apaper+not%3Afunny", true);
     xhttp.send();
 
-    
+
 }
