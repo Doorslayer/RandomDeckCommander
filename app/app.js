171fabe9a941b1f6ea33f37fb720714b140a6b6d
Doorslayer <59379406+Doorslayer@users.noreply.github.com>
2024-01-17 10:24:30 -0800

    Added log message to nextCard function

--- app/app.js
+++ app/app.js
@@ -19,6 +19,7 @@ function nextCard() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let card = JSON.parse(xhttp.responseText);
+            console.log(card.name);
             sessionStorage.setItem('card', card.name);
             document.getElementById("cardImage").src = card.image_uris.normal;
         }
