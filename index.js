da41dac61f8be4174805337ae4f61359ee9631ca
Doorslayer <59379406+Doorslayer@users.noreply.github.com>
2024-03-29 19:57:13 -0700

    Added commander mode

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
 //Author: Kaylehb Edward T. States
-//Date: 2023-04-27
+//Date: 2024-03-29
 window.onload = setDefaultValues;
 
 
 function setDefaultValues() {
-    const DEFAULT_SKIPS = 5;
-    const DEFAULT_DOUBLES = 4;
-    const DEFAULT_TRIPLES = 3;
-    const DEFAULT_PLAYSETS = 2;
+    const DEFAULT_COPTIONS = 5;
+    const DEFAULT_NOPTIONS = 3;
 
     // Get the saved values from localStorage, or use the default values
-    localStorage.setItem('skips', localStorage.getItem('skips') || DEFAULT_SKIPS);
-    localStorage.setItem('doubles', localStorage.getItem('doubles') || DEFAULT_DOUBLES);
-    localStorage.setItem('triples', localStorage.getItem('triples') || DEFAULT_TRIPLES);
-    localStorage.setItem('playsets', localStorage.getItem('playsets') || DEFAULT_PLAYSETS);
+    localStorage.setItem('COptions', localStorage.getItem('COptions') || DEFAULT_COPTIONS);
+    localStorage.setItem('NOptions', localStorage.getItem('NOptions') || DEFAULT_NOPTIONS);
 
     // Update the text content of the HTML elements
-    document.getElementById('SkipsVal').textContent = localStorage.getItem('skips');
-    document.getElementById('DoublesVal').textContent = localStorage.getItem('doubles');
-    document.getElementById('TriplesVal').textContent = localStorage.getItem('triples');
-    document.getElementById('PlaysetsVal').textContent = localStorage.getItem('playsets');
+    document.getElementById('COptionsVal').textContent = localStorage.getItem('COptions');
+    document.getElementById('NOptionsVal').textContent = localStorage.getItem('NOptions');
+
 
 }
 
-function incrementSkips(step) {
-    localStorage.setItem('skips', parseInt(localStorage.getItem('skips')) + step);
-    document.getElementById('SkipsVal').textContent = localStorage.getItem('skips');
+function incrementCOptions(step) {
+    localStorage.setItem('COptions', parseInt(localStorage.getItem('COptions')) + step);
+    document.getElementById('COptionsVal').textContent = localStorage.getItem('COptions');
 }
 
 function incrementDoubles(step) {
-    localStorage.setItem('doubles', parseInt(localStorage.getItem('doubles')) + step);
-    document.getElementById('DoublesVal').textContent = localStorage.getItem('doubles');
-}
-
-function incrementTriples(step) {
-    localStorage.setItem('triples', parseInt(localStorage.getItem('triples')) + step);
-    document.getElementById('TriplesVal').textContent = localStorage.getItem('triples');
-}
-
-function incrementPlaysets(step) {
-    localStorage.setItem('playsets', parseInt(localStorage.getItem('playsets')) + step);
-    document.getElementById('PlaysetsVal').textContent = localStorage.getItem('playsets');
+    localStorage.setItem('NOptions', parseInt(localStorage.getItem('NOptions')) + step);
+    document.getElementById('NOptionsVal').textContent = localStorage.getItem('NOptions');
 }
